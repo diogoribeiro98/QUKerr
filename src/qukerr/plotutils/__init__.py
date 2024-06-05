ecfc4ab7d9a7ddf37a80b9f6c3bb4d5485c29fd1
Diogo Ribeiro <diogo.gnc.ribeiro@gmail.com>
2024-06-05 20:17:55 +0200

    added standard plot colors to plotutils

--- src/qukerr/plotutils/__init__.py
+++ src/qukerr/plotutils/__init__.py
@@ -1,2 +1,3 @@
+from .colors import clist
 from .cross_plot import cross_plot
 from .setup_nice_plots import create_mpl_theme
---
+++ src/qukerr/plotutils/colors.py
@@ -0,0 +1,11 @@
+clist = {
+    'blue2'          : '#3288bd' , 
+    'green2'        : '#99d594' ,
+    'light green'   : '#e6f598' ,
+    'yellow'        : '#fee08b' ,
+    'orange'        : '#fc8d59' ,
+    'red'           : '#d53e4f' ,
+    'purple'        : '#984ea3' ,
+    'green'         : '#5ab557' ,
+    'blue'          : '#377eb8'
+    }
